7954f90b1fd4e916df7c0a57617bf13c2155a410
Caio R. Tavares <159586506+caiortavares@users.noreply.github.com>
2024-07-16 09:53:11 -0300

    Inclusão da classe que testa os "prints"

---
+++ FormasImpressoes.java
@@ -0,0 +1,20 @@
+public class Teste {
+    public static void main(String[] args){
+        int x = 2;
+        int y = 3;
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+
+        System.out.printf("%d = %d%n", (x + y), (y + x));
+        System.out.println(a + " " + b + " " + c + " " + d);
+        System.out.print(a + " " );
+        System.out.print(b + " " );
+        System.out.print(c + " " );
+        System.out.print(d);
+        System.out.printf("\n %d %d %d %d", a, b, c, d);
+        
+    }
+
+}
