e2cc7ed01fbe5caa0d6e103e131c9fa75354a0d3
Caio R. Tavares <159586506+caiortavares@users.noreply.github.com>
2024-07-17 14:25:35 -0300

    formas asteriscos

---
+++ FormasAsteriscos.java
@@ -0,0 +1,24 @@
+public class FormasAsteriscos {
+    public static void main(String[] args) {
+        System.out.print("*********\t\t   ***   \t\t   *  \t\t    *\n");
+        System.out.print("*       *\t\t *     * \t\t  *** \t\t   * *\n");
+        System.out.print("*       *\t\t*       *\t\t *****\t\t  *   *\n");
+        System.out.print("*       *\t\t*       *\t\t   *  \t\t *     *\n");
+        System.out.print("*       *\t\t*       *\t\t   *  \t\t*       *\n");
+        System.out.print("*       *\t\t*       *\t\t   *  \t\t *     *\n");
+        System.out.print("*       *\t\t*       *\t\t   *  \t\t  *   *\n");
+        System.out.print("*       *\t\t *     * \t\t   *  \t\t   * *\n");
+        System.out.print("*********\t\t   ***   \t\t   *  \t\t    *\n");
+
+        System.out.println(" ");
+        System.out.print("********\t\t    ***    \t\t   *   \t         *          \n");
+        System.out.print("*      *\t\t  *     *  \t\t  ***  \t       *   *        \n");
+        System.out.print("*      *\t\t*         *\t\t ***** \t     *       *      \n");
+        System.out.print("*      *\t\t*         *\t\t   *   \t   *           *    \n");
+        System.out.print("*      *\t\t*         *\t\t   *   \t *               *  \n");
+        System.out.print("*      *\t\t*         *\t\t   *   \t   *           *    \n");
+        System.out.print("*      *\t\t*         *\t\t   *   \t     *       *      \n");
+        System.out.print("*      *\t\t  *     *  \t\t   *   \t       *   *        \n");
+        System.out.print("********\t\t    ***    \t\t   *   \t         *          \n");
+    }
+}
