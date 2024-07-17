addb9b5b96290c15faa5c55bf103080b444075cc
Caio R. Tavares <159586506+caiortavares@users.noreply.github.com>
2024-07-17 15:05:34 -0300

    atualização exercicio

--- FormasAsteriscos.java
+++ FormasAsteriscos.java
@@ -8,17 +8,12 @@ public class FormasAsteriscos {
         System.out.print("*       *\t\t*       *\t\t   *  \t\t *     *\n");
         System.out.print("*       *\t\t*       *\t\t   *  \t\t  *   *\n");
         System.out.print("*       *\t\t *     * \t\t   *  \t\t   * *\n");
-        System.out.print("*********\t\t   ***   \t\t   *  \t\t    *\n");
+        System.out.println("*********\t\t   ***   \t\t   *  \t\t    *\n");
 
-        System.out.println(" ");
-        System.out.print("********\t\t    ***    \t\t   *   \t         *          \n");
-        System.out.print("*      *\t\t  *     *  \t\t  ***  \t       *   *        \n");
-        System.out.print("*      *\t\t*         *\t\t ***** \t     *       *      \n");
-        System.out.print("*      *\t\t*         *\t\t   *   \t   *           *    \n");
-        System.out.print("*      *\t\t*         *\t\t   *   \t *               *  \n");
-        System.out.print("*      *\t\t*         *\t\t   *   \t   *           *    \n");
-        System.out.print("*      *\t\t*         *\t\t   *   \t     *       *      \n");
-        System.out.print("*      *\t\t  *     *  \t\t   *   \t       *   *        \n");
-        System.out.print("********\t\t    ***    \t\t   *   \t         *          \n");
+        System.out.printf("*%n**%n***%n****%n*****%n%n");
+
+        System.out.printf("%s%n%s%n%s%n", "*", "***", "*****");
+
+
     }
 }
